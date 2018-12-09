7e6ace66a1c05d0136bd813ea43acd1b337223a2
jmthompson2015 <softwaredev@jeffreythompson.net>
2018-12-09 14:10:17 -0700

    Initial version.

---
+++ research-tree/app/ResearchTree.js
@@ -0,0 +1,18 @@
+import ActionCreator from "../model/ActionCreator.js";
+import Reducer from "../model/Reducer.js";
+
+import InputPanelContainer from "../container/InputPanelContainer.js";
+import ResearchTableContainer from "../container/ResearchTableContainer.js";
+
+const store = Redux.createStore(Reducer.root);
+store.dispatch(ActionCreator.setArea("PHYSICS"));
+store.dispatch(ActionCreator.setCategory("PARTICLES"));
+store.dispatch(ActionCreator.setResearch("tech_lasers_4"));
+
+const container0 = React.createElement(InputPanelContainer);
+const element0 = React.createElement(ReactRedux.Provider, { store }, container0);
+ReactDOM.render(element0, document.getElementById("inputPanel"));
+
+const container1 = React.createElement(ResearchTableContainer);
+const element1 = React.createElement(ReactRedux.Provider, { store }, container1);
+ReactDOM.render(element1, document.getElementById("researchPanel"));
