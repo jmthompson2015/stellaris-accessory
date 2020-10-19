e436f00914a31338fa808480b123d2cda1763d14
Jeffrey M. Thompson <jmthompson2015@users.noreply.github.com>
2020-10-19 09:15:44 -0600

    Updated.

--- building-table/BuildingTable.js
+++ building-table/BuildingTable.js
@@ -1,13 +1,28 @@
+/* eslint no-console: ["error", { allow: ["log"] }] */
+
 import Building from "../artifact/Building.js";
 
 import TableColumns from "./TableColumns.js";
 
 const tableRows = Object.values(Building);
+const appName = "SABuildingTable";
+const onFilterChange = filteredTableRows => {
+  console.log(`onFilterChange() filteredTableRows.length = ${filteredTableRows.length}`);
+};
+const onShowColumnChange = columnToChecked => {
+  console.log(`onShowColumnChange() columnToChecked = ${JSON.stringify(columnToChecked)}`);
+};
+const isVerbose = true;
 
-const frt = new FilteredReactTable(TableColumns, tableRows);
+const frt = new FilteredReactTable(
+  TableColumns,
+  tableRows,
+  appName,
+  onFilterChange,
+  onShowColumnChange,
+  isVerbose
+);
 
-const filter = frt.filterElement();
-ReactDOM.render(filter, document.getElementById("filter"));
-
-const table = frt.tableElement();
-ReactDOM.render(table, document.getElementById("table"));
+ReactDOM.render(frt.filterPanel(), document.getElementById("filter"));
+ReactDOM.render(frt.showColumnsPanel(), document.getElementById("showColumns"));
+ReactDOM.render(frt.tableElement(), document.getElementById("table"));
